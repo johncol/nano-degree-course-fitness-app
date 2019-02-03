e11a6a01205eb8d9c11919bc66b5b3df514e65e2
John <john.19col@gmail.com>
2019-02-03 12:00:01 -0500

    Export all colors in a COLOR constant instead of each one individually

--- src/js/utils/colors.js
+++ src/js/utils/colors.js
@@ -1,8 +1,19 @@
-export const purple = '#292477';
-export const gray = '#757575';
-export const white = '#fff';
-export const red = '#b71845';
-export const orange = '#f26f28';
-export const blue = '#4e4cb8';
-export const lightPurp = '#7c53c3';
-export const pink = '#b93fb3';
+const purple = '#292477';
+const gray = '#757575';
+const white = '#fff';
+const red = '#b71845';
+const orange = '#f26f28';
+const blue = '#4e4cb8';
+const lightPurp = '#7c53c3';
+const pink = '#b93fb3';
+
+export const COLOR = {
+  purple,
+  gray,
+  white,
+  red,
+  orange,
+  blue,
+  lightPurp,
+  pink
+};
