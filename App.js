3320e0e75827e950bb8ab86e7045205704c34b99
John <john.19col@gmail.com>
2019-02-09 22:28:14 -0500

    Add Ionicons element

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Fitness app built with react native!</Text>
+        <Ionicons name="ios-power" size={50} color="#990000" />
       </View>
     );
   }
