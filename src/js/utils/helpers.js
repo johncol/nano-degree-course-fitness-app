9fa29f4c4218f880378502c38ba38f21d8300b85
John <john.19col@gmail.com>
2019-02-09 22:42:40 -0500

    Expose getMetricsMetaInfo method in helpers file

--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+import { MaterialIcons, FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
+
 export const isBetween = (num, x, y) => {
   return num >= x && num <= y;
 };
@@ -36,4 +40,70 @@ export const timeToString = (time = Date.now()) => {
     Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
   );
   return todayUTC.toISOString().split('T')[0];
+};
+
+export const getMetricsMetaInfo = metric => {
+  const info = {
+    run: {
+      displayName: 'Run',
+      max: 50,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon: () => (
+        <View>
+          <MaterialIcons name="directions-run" color="black" size={35} />
+        </View>
+      )
+    },
+    bike: {
+      displayName: 'Bike',
+      max: 100,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon: () => (
+        <View>
+          <MaterialCommunityIcons name="bike" color="black" size={35} />
+        </View>
+      )
+    },
+    swim: {
+      displayName: 'Swim',
+      max: 9900,
+      unit: 'meter',
+      step: 100,
+      type: 'steppers',
+      getIcon: () => (
+        <View>
+          <MaterialCommunityIcons name="swim" color="black" size={35} />
+        </View>
+      )
+    },
+    sleep: {
+      displayName: 'Sleep',
+      max: 24,
+      unit: 'hours',
+      step: 1,
+      type: 'slider',
+      getIcon: () => (
+        <View>
+          <FontAwesome name="bed" color="black" size={35} />
+        </View>
+      )
+    },
+    eat: {
+      displayName: 'Eat',
+      max: 10,
+      unit: 'rating',
+      step: 1,
+      type: 'slider',
+      getIcon: () => (
+        <View>
+          <MaterialCommunityIcons name="food" color="black" size={35} />
+        </View>
+      )
+    }
+  };
+  return metric ? info[metric] : metric;
 };
