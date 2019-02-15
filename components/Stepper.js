02ad8c3381669fcdae6f12c3c456061eaa90cc88
John <john.19col@gmail.com>
2019-02-15 12:31:45 -0500

    Add Slider and Stepper (empty) components

---
+++ components/Stepper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Stepper = () => {
+  return (
+    <View>
+      <Text>Stepper component</Text>
+    </View>
+  );
+};
+
+export default Stepper;
