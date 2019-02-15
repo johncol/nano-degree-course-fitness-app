a6c2e326b07ab9036f2b45e66018a1826e310347
John <john.19col@gmail.com>
2019-02-15 15:52:51 -0500

    Create Slider component using as base the React Native Slider

--- components/Slider.js
+++ components/Slider.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Slider as ReactNativeSlider } from 'react-native';
 
-const Slider = () => {
+const Slider = ({ step, value, onChange, max, unit }) => {
   return (
     <View>
-      <Text>Slider component</Text>
+      <ReactNativeSlider
+        step={step}
+        value={value}
+        minimumValue={0}
+        maximumValue={max}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 };
