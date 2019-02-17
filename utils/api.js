bd9523cabcd9cc27418df6e4db264dfd980801d6
John <john.19col@gmail.com>
2019-02-17 16:52:24 -0500

    Add api.js file with sumbitEntry and removeEntry methods

---
+++ utils/api.js
@@ -0,0 +1,18 @@
+import { AsyncStorage } from 'react-native';
+import { CALENDAR_STORAGE_KEY } from './calendar';
+
+export const sumbitEntry = (key, entry) => {
+  const newEntry = JSON.stringify({
+    [key]: entry
+  });
+  return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, newEntry);
+};
+
+export const removeEntry = key => {
+  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY).then(storageItem => {
+    const data = JSON.parse(storageItem);
+    data[key] = undefined;
+    delete data[key];
+    AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
+  });
+};
