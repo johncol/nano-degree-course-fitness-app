26ae03f3424dd384d2c74e14d2d85ef1375a4e6e
John <john.19col@gmail.com>
2019-02-18 17:49:34 -0500

    Wrap application with the provider of the react store

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import { createStore } from 'redux';
+import reducers from './reducers';
+
 import AddEntry from './components/AddEntry';
+
+const store = createStore(reducers);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <AddEntry />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <AddEntry />
+        </View>
+      </Provider>
     );
   }
 }
