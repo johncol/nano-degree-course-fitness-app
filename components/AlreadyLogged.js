6aad0a1305290415d5e575539cded82348f21f29
John <john.19col@gmail.com>
2019-02-19 11:04:36 -0500

    Style AlreadyLogged component

--- components/AlreadyLogged.js
+++ components/AlreadyLogged.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 import TextButton from './TextButton';
 
 const AlreadyLogged = ({ onReset }) => {
   return (
-    <View>
+    <View style={style.container}>
       <Ionicons name="ios-fitness" size={100} />
-      <Text>You already logged your information for today</Text>
-      <TextButton onPress={onReset}>Reset</TextButton>
+      <Text style={style.text}>You already logged your information for today</Text>
+      <TextButton onPress={onReset} buttonStyle={style.button}>
+        Reset
+      </TextButton>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 20,
+    marginTop: 20
+  },
+  button: {
+    marginTop: 30
+  }
+});
 
 export default AlreadyLogged;
