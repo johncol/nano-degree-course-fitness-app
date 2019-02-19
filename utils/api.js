4e5f42fdcff27c1f239e49ad323210a1deeefaea
John <john.19col@gmail.com>
2019-02-19 16:24:15 -0500

    Add fetchCalendarResults method to API file

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
 import { AsyncStorage } from 'react-native';
-import { CALENDAR_STORAGE_KEY } from './calendar';
+import { CALENDAR_STORAGE_KEY, formatCalendarResults } from './calendar';
 
 export const sumbitEntry = (key, entry) => {
   const newEntry = JSON.stringify({
@@ -15,4 +15,8 @@ export const removeEntry = key => {
     delete data[key];
     AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
   });
+};
+
+export const fetchCalendarResults = () => {
+  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY).then(formatCalendarResults);
 };
