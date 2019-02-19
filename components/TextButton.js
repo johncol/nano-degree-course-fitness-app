f1429b7147a8f9999abce285ecd974dccdc2e700
John <john.19col@gmail.com>
2019-02-19 11:04:57 -0500

    Add default styles to TextButton component

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const TextButton = ({ onPress, children }) => {
+import { COLOR } from '../utils/colors';
+
+const TextButton = ({ onPress, children, buttonStyle, buttonTextStyle }) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>{children}</Text>
+    <TouchableOpacity onPress={onPress} style={[style.button, buttonStyle]}>
+      <Text style={[style.buttonText, buttonTextStyle]}>{children}</Text>
     </TouchableOpacity>
   );
 };
+
+const style = StyleSheet.create({
+  button: {
+    backgroundColor: COLOR.purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: COLOR.white,
+    fontSize: 22,
+    textAlign: 'center'
+  }
+});
 
 export default TextButton;
