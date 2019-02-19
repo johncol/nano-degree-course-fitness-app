5bb700ef47a545827c997efa7428308d721ec8a6
John <john.19col@gmail.com>
2019-02-19 11:05:09 -0500

    Style the DateHeader component

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
+import { COLOR } from '../utils/colors';
 
 const DateHeader = ({ date }) => {
-  return <Text>{date}</Text>;
+  return <Text style={style.text}>{date}</Text>;
 };
+
+const style = StyleSheet.create({
+  text: {
+    color: COLOR.purple,
+    fontSize: 35,
+    paddingTop: 10
+  }
+});
 
 export default DateHeader;
