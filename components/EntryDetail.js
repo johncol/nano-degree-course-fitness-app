42ee41fe22d8be5494d37874ea69e4c035a57b11
John <john.19col@gmail.com>
2019-02-23 16:21:20 -0500

    Show date key value as the entry detail view title in the navigation bar

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const EntryDetail = ({ navigation }) => {
-  return (
-    <View>
-      <Text>Entry detail component for entry {navigation.state.params.entryId}</Text>
-    </View>
-  );
-};
+class EntryDetail extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    const { entryId: title } = navigation.state.params;
+    return { title };
+  };
+
+  render() {
+    const { navigation } = this.props;
+    return (
+      <View>
+        <Text>Entry detail component for entry {navigation.state.params.entryId}</Text>
+      </View>
+    );
+  }
+}
 
 export default EntryDetail;
