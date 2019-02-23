fefc8943cc8f3b3806e33843cc07512305d92d56
John <john.19col@gmail.com>
2019-02-23 16:08:23 -0500

    Fix styles error: cannot be extracted as they depend on a prop

--- components/StatusBar.js
+++ components/StatusBar.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { View, StatusBar as ReactNativeStatusBar, StyleSheet } from 'react-native';
+import { View, StatusBar as ReactNativeStatusBar } from 'react-native';
 import { Constants } from 'expo';
 
 const StatusBar = props => {
   return (
-    <View style={style.container}>
+    <View
+      style={{
+        backgroundColor: props.backgroundColor,
+        height: Constants.statusBarHeight
+      }}
+    >
       <ReactNativeStatusBar translucent {...props} />
     </View>
   );
 };
-
-const style = StyleSheet.create({
-  container: {
-    backgroundColor: props.backgroundColor,
-    height: Constants.statusBarHeight
-  }
-});
 
 export default StatusBar;
