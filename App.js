a6676459d4ecf18a48adc999c5ba537e78facae6
John <john.19col@gmail.com>
2019-02-23 16:14:16 -0500

    Use react-navigation createAppContainer method to create the MainApp
    The MainApp component is the one that should be rendered instead of the AppNavigation component in react-navigation v3

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { createAppContainer } from 'react-navigation';
 
 import { createStore } from 'redux';
 import reducers from './reducers';
@@ -11,13 +12,15 @@ import { COLOR } from './utils/colors';
 
 const store = createStore(reducers);
 
+const MainApp = createAppContainer(AppNavigation);
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <View style={styles.container}>
           <StatusBar backgroundColor={COLOR.purple} barStyle="light-content" />
-          <AppNavigation />
+          <MainApp />
         </View>
       </Provider>
     );
