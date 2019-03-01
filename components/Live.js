9f2d562ead6b84432d466c83aa5e2de08ab0cb83
John <john.19col@gmail.com>
2019-03-01 15:45:38 -0500

    Add implementation for the PermissionsUndetermined component

--- components/Live.js
+++ components/Live.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+import Foundation from '@expo/vector-icons/Foundation';
 
 import { Permissions } from './../constants/Permissions';
+import { COLOR } from './../utils/colors';
 
 const PermissionsDenied = () => (
   <View>
@@ -9,29 +17,38 @@ const PermissionsDenied = () => (
   </View>
 );
 
-const PermissionsUndetermined = () => (
-  <View>
-    <Text>{Permissions.UNDETERMINED}</Text>
+const PermissionsUndetermined = ({ onEnableLocation: askForLocationPermission }) => (
+  <View style={style.center}>
+    <Foundation name="alert" size={50} />
+    <Text>You need to enable location services for this app.</Text>
+    <TouchableOpacity style={style.button} onPress={askForLocationPermission}>
+      <Text style={style.buttonText}>Enable</Text>
+    </TouchableOpacity>
   </View>
 );
 
 class Live extends Component {
   state = {
-    permissions: null,
+    permissions: Permissions.UNDETERMINED,
     coords: null,
     direction: null
   };
 
+  askForLocationPermission = () => {};
+
   render() {
     const { permissions } = this.state;
+
     if (!permissions) {
       return <ActivityIndicator style={style.activityIdicator} />;
     }
+
     if (permissions === Permissions.DENIED) {
       return <PermissionsDenied />;
     }
+
     if (permissions === Permissions.UNDETERMINED) {
-      return <PermissionsUndetermined />;
+      return <PermissionsUndetermined onEnableLocation={this.askForLocationPermission} />;
     }
 
     return (
@@ -45,6 +62,28 @@ class Live extends Component {
 const style = StyleSheet.create({
   activityIdicator: {
     marginTop: 30
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30
+  },
+  button: {
+    padding: 10,
+    margin: 20,
+    backgroundColor: COLOR.purple,
+    alignSelf: 'center',
+    borderRadius: 5
+  },
+  buttonText: {
+    color: COLOR.white,
+    fontSize: 20
   }
 });
 
